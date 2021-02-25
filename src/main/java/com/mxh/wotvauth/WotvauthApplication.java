0185575f608d4d0184e2763b619ed12941f12835
Thilin <fernando_cauper@hotmail.com>
2021-02-25 08:12:48 -0400

    add: first commit

---
+++ src/main/java/com/mxh/wotvauth/WotvauthApplication.java
@@ -0,0 +1,13 @@
+package com.mxh.wotvauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotvauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WotvauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mxh/wotvauth/WotvauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxh.wotvauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotvauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
